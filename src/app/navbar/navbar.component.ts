23c6a3313bab9382c4b5b73ddd0768c561d59958
Harsh Verma <hv41096@gmail.com>
2024-01-10 16:40:56 +0530

    Text typing navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -20,5 +20,35 @@ animation(){
 }
 ngOnInit(): void {
     this.animation()
+
+    const dynamicText = document.querySelector('h4 span');
+    // dynamicText?.classList.add('stop-blink')
+    const words= ['Programmer', 'Freelancer', 'Problem Solver', 'Designer'];
+    let wordIndex = 0;
+    let charIndex =0;
+    let isDeleting = false;
+    const typeEffect = ()=>{
+      const curentWord = words[wordIndex];
+      const currentChar = curentWord.substring(0, charIndex)
+      dynamicText!.textContent=currentChar
+      dynamicText?.classList.add('stop-blink')
+
+      if(!isDeleting && charIndex < curentWord.length){
+        charIndex++;
+        setTimeout(typeEffect, 200);
+      }
+      else if(isDeleting && charIndex >0){
+        charIndex--;
+        setTimeout(typeEffect, 100);
+      }
+      else{
+        isDeleting = !isDeleting;
+        dynamicText?.classList.remove('stop-blink')
+        wordIndex = !isDeleting ? (wordIndex+1)%words.length : wordIndex;
+        setTimeout(typeEffect,1200)
+      }
+    }
+    // typeEffect()
+    setTimeout(typeEffect,850);
 }
 }
