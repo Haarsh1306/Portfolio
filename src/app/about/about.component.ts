c0d25df9723c336ab9f15c5e3ec0cf4ec35a9f24
Harsh Verma <hv41096@gmail.com>
2024-07-18 23:27:22 +0530

    about update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   about: string = `I'm Harsh Verma, a final year B.Tech. student in Computer Engineering with a specialization in Data Science. I'm also a passionate software developer with experience as a Software Engineer Trainee at Chromeinfotech, Noida.
 
-  As a full-stack MEAN developer, I enjoy crafting user-friendly and data-driven web applications.In my free time, I build personal projects to explore new technologies and hone my skills. I'm particularly interested in full-stack development.
+  As a full-stack MERN developer, I enjoy crafting user-friendly and data-driven web applications.In my free time, I build personal projects to explore new technologies and hone my skills. I'm particularly interested in full-stack development.
 
   I'm always eager to learn and grow as a developer. If you're looking for a talented and enthusiastic full-stack developer, I'd love to connect ! Feel free to reach out to me at or connect with me on LinkedIn.`.replace(/\n/g, '<br>');
   constructor(){
